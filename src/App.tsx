fdc2205f6f79d4314eb9452ea940cbe6273e83ac
Kheder Taleb <ktaleb@innovura.io>
2024-08-28 15:30:14 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Calendar } from "./components/Calendar";
+import { EventsProvider } from "./context/Events";
+import "./styles.css";
+
+function App() {
+  return (
+    <EventsProvider>
+      <Calendar />
+    </EventsProvider>
+  );
+}
+
+export default App;
